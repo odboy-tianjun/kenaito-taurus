f3b8eb1641b85ab101725cabf35d9bda804bf399
Odboy <tianjun@odboy.cn>
2025-01-13 18:54:00 +0800

    by zhengjie
    修复EncryptUtils多线程环境中共享同一个 Cipher实例导致【 Cipher not initialized】的问题

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/security/security/JwtAuthenticationEntryPoint.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/security/security/JwtAuthenticationEntryPoint.java
@@ -15,6 +15,9 @@
  */
 package me.zhengjie.modules.security.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.zhengjie.infra.exception.handler.ApiError;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -34,6 +37,11 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
         // 当用户尝试访问安全的REST资源而不提供任何凭据时，将调用此方法发送401 响应
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException==null?"Unauthorized":authException.getMessage());
+        int code = HttpStatus.UNAUTHORIZED.value();
+        response.setStatus(code);
+        response.setContentType("application/json;charset=UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(ApiError.error(HttpStatus.UNAUTHORIZED.value(), "登录状态已过期，请重新登录"));
+        response.getWriter().write(jsonResponse);
     }
 }
